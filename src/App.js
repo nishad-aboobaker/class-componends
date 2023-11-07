50b9d7b35f2763f882245b116f97ed0d3ae46f26
Nishad <nishuanshad@gmail.com>
2023-11-07 10:52:18 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+///////class components///////
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>hello heyyy hoiii ooiii</h1>
+//       <Second/>
+//     </div>
+//   );
+// }
+
+
+// class Second extends Component{
+//   render(){
+//     return(
+//       <div style={{"background-Color":"red"}}>
+//         <h1>hello</h1>
+//         <h2>hiiiiii</h2>
+//       </div>
+//     )
+//   }
+// }
+
+
+
+
+////////state,set-state//////////
+
+
+class App extends Component{
+  state={
+    value:"Synnefo"
+  }
+
+  clk=()=>{
+    this.setState({
+      value:"Novavi"
+    })
+  }
+
+  render(){
+    return(
+      <div style={{"background-color":"red"}}>
+
+        <h1>hello</h1>
+        <h2>{this.state.value}</h2>
+        <button onClick={this.clk}>click</button>
+
+      </div>
+    )
+  }
+
+
 }
+
+
+
+
+
+
+
+
 
 export default App;
